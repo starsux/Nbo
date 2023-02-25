6659406d51bcfdfabc2ee25d101229073b124afc
StarsUx <82619416+starsux@users.noreply.github.com>
2023-02-25 16:37:51 -0600

    class removed

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import os
 
 TOKEN = os.getenv('TOKEN')
 
-class BotClient(discord.Client):
-    client = self
-    command_char = "/"
-    async def on_ready():
-        print(f'{client.user} has connected to discord')
-
-    async def on_message(msg):
-        # To avoid bot message
-        if(msg.author == client.user): return
-
-        if(msg.content.startswith(comand_char + "help")):
-            await msg.channel.send("No")
-
-
 intents = discord.Intents.default()
-client = BotClient(intents=intents)
-client.run(TOKEN)
+client = discord.Client(intents=intents)
+
+command_char = "/"
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to discord')
+
+@client.event
+async def on_message(msg):
+    # To avoid bot message
+    if(msg.author == client.user): return
+
+    if(msg.content.startswith(comand_char + "help")):
+        await msg.channel.send("No")
+
