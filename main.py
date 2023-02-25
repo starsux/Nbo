6a7a77610f23833d95e6af532488363c05c9831f
StarsUx <82619416+starsux@users.noreply.github.com>
2023-02-25 16:46:58 -0600

    msg

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def on_message(msg):
     # To avoid bot message
     if(msg.author == client.user): return
 
-    if(msg.content.startswith(comand_char + "/help")):
+    if(msg.content.startswith("/help")):
         await msg.channel.send("No")
 
 client.run(TOKEN)
