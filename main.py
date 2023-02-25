a85a6881969a39bb7f037b012675ecd3c37425e1
StarsUx <82619416+starsux@users.noreply.github.com>
2023-02-25 16:44:08 -0600

    msg

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-command_char = "/"
-
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to discord')
@@ -19,7 +17,7 @@ async def on_message(msg):
     # To avoid bot message
     if(msg.author == client.user): return
 
-    if(msg.content.startswith(comand_char + "help")):
+    if(msg.content.startswith(comand_char + "/help")):
         await msg.channel.send("No")
 
 client.run(TOKEN)
