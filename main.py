7844c642be10ed02cf94d551fb33cb4d27f592a1
StarsUx <82619416+starsux@users.noreply.github.com>
2023-02-25 16:34:43 -0600

    help command

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ import os
 TOKEN = os.getenv('TOKEN')
 
 class BotClient(discord.Client):
-    async def on_ready(self):
-        print(f'{self.user} has connected to discord')
+    client = self
+    command_char = "/"
+    async def on_ready():
+        print(f'{client.user} has connected to discord')
+
+    async def on_message(msg):
+        # To avoid bot message
+        if(msg.author == client.user): return
+
+        if(msg.content.startswith(comand_char + "help")):
+            await msg.channel.send("No")
 
 
 intents = discord.Intents.default()
